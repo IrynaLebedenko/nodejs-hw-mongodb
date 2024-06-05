a2752a2d6b03086bdca11f01b08c88abcfdd769e
IrynaLebedenko <94801312+IrynaLebedenko@users.noreply.github.com>
2024-06-05 16:47:09 +0300

    fixed hw

--- src/controllers/contactById.js
+++ src/controllers/contactById.js
@@ -6,10 +6,12 @@ export const getContact = async (req, res) => {
     try{
         const contact = await getContactById(contactId);
         if (!contact) {
-            
+
             return res.status(404).json({
+
                 status: '404',
                 message: 'Not found',
+
             });
         }
         res.status(200).json({
@@ -17,6 +19,7 @@ export const getContact = async (req, res) => {
             message: `Successfully found contact with id ${contactId}!`,
              data: contact,
         });
+
     }catch (error){
         res.status(500).json({
             status: 'error',
